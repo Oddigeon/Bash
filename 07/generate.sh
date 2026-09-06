5f612ebdcb5de8b36bbd845187484ef4cab31ff2
Daniil <93969680+Oddigeon@users.noreply.github.com>
2026-09-06 21:56:06 +0300

    7.3 commit

--- 07/generate.sh
+++ 07/generate.sh
@@ -2,26 +2,29 @@
 
 generate() {
 	local count j
+	local filename=$1 count_day=$2
 	count=$((100 + RANDOM % 901))
 	for (( f=1; f<=count; f++ )); do
 		ip=$(shuf -i 1-255 -n 1).$(shuf -i 1-255 -n 1).$(shuf -i 1-255 -n 1).$(shuf -i 1-255 -n 1)
 
 		http_codes=(200 201 400 401 403 404 500 501 502 503)
-		index_code=$((RANDOM % ${#http_codes}))
+		index_code=$((RANDOM % ${#http_codes[@]}))
 		code=${http_codes[$index_code]}
 
 		http_methods=(GET POST PUT PATCH DELETE)
-		index_method=$((RANDOM % ${#http_methods}))
+		index_method=$((RANDOM % ${#http_methods[@]}))
 		method=${http_methods[$index_method]}
 
-		date=$(date '+%a')
+		day=$(($(date '+%d') + $count_day))
+		date=$(date '+/%b/%Y:%H:%M:%S')
+		final_date="$day$date"
 
-		url="https://page_$((RANDOM % 1000))"
+		url="/page_$((RANDOM % 1000))"
 
 		agents=("Mozilla" "Google Chrome" "Opera" "Safari" "Internet Explorer" "Microsoft Edge" "Crawler and bot" "Library and net tool")
-		index_agent=$((RANDOM %${#agents}))
+		index_agent=$((RANDOM %${#agents[@]}))
 		agent=${agents[$index_agent]}
 
-		echo "$ip -- [$date +0000] $code $method $url $agent" >> $filename
+		echo "$ip - - [$final_date +0000] $code \"$method $url HTTP/1.1\" \"-\" $agent" >> $filename
 	done
 }
